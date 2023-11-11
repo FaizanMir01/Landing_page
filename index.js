475ae85775d48f8d8061ff6df0caa27f8dcb9df0
Faizan Manzoor Mir <141254735+FaizanMir01@users.noreply.github.com>
2023-11-11 18:16:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+
+ Shery.mouseFollower();
+ Shery.makeMagnet(".left img");
+ Shery.makeMagnet("#learn,a,p");
+ Shery.hoverWithMediaCircle(" h1",{images:["IMG_7567.JPG","IMG_7566.JPG","IMG_7568.JPG"]});
